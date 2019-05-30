36adc48be0b2cd21fc8bd64ce1a43df7e61ec041
Wutj <b04901104@ntu.edu.tw>
2019-05-30 17:26:35 +0800

    mv analysis to googledrive

--- random_forest.py
+++ random_forest.py
@@ -10,16 +10,15 @@ import os
 
 def main():
     label_handler = LabelHandler('data/2015-2016/Questionnaire.txt')
-    f = open('analyze_files/acc.txt', 'w')
+    os.makedirs('../analyze_files', exist_ok=True)
+    f = open('../analyze_files/acc.txt', 'w')
     #feature_selected = ['ALQ','DUQ','SMQ','DPQ','PAQ'] #choose some data
     #feature_selected = []  #use all data
     category = label_handler.get_categories()
-    for cat in range(0,len(category),2):
+    for cat in range(0,len(category),1):
         try:
-            os.makedirs('analyze_files/'+category[cat]+' and '+category[cat+1] , exist_ok=True)
-            feature_selected = []
-            for i in range (2):
-                feature_selected += label_handler.get_symbols_by_category(category[cat+i])
+            os.makedirs('../analyze_files/'+category[cat], exist_ok=True)
+            feature_selected = label_handler.get_symbols_by_category(category[cat])
             print('feature_selected: ',feature_selected)
             data = get_2015_Quesitonaire_data(feature_selected)
             target_feature = label_handler.get_content_by_symbol('SLQ050')
@@ -38,10 +37,10 @@ def main():
             print('Training score: ', train_score)
             print('Training score: ', valid_score)
             print('=======analyze=======')
-            generate_tree_png(model.estimators_[0], data.columns, target_feature,'analyze_files/'+category[cat]+' and '+category[cat+1]+'/tree.png')
-            permutation_importance(model, x_valid, y_valid, 'analyze_files/'+category[cat]+' and '+category[cat+1]+'/permutation_importance.csv')
-            #partial_dependence_plot('Avg # alcoholic drinks/day - past 12 mos', model, x_valid, data.columns,'analyze_files/partial_dependence_plot.png')
-            shap_plot(model, x_valid, 'analyze_files/'+category[cat]+' and '+category[cat+1]+'/')
+            generate_tree_png(model.estimators_[0], data.columns, target_feature,'../analyze_files/'+category[cat]+'/tree.png')
+            permutation_importance(model, x_valid, y_valid, '../analyze_files/'+category[cat]+'/permutation_importance.csv')
+            #partial_dependence_plot('Avg # alcoholic drinks/day - past 12 mos', model, x_valid, data.columns,'../analyze_files/partial_dependence_plot.png')
+            shap_plot(model, x_valid, '../analyze_files/'+category[cat]+'/')
             f.write('================================'+"\n")
             f.write('Target feature:'+str(target_feature)+"\n")
             f.write(category[cat]+' and '+category[cat+1]+"\n")
