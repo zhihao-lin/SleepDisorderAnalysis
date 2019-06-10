ef3ac933aabd2bf141f70f1ed2e164a8b21cc75b
James Lin <b04901069@ntu.edu.tw>
2019-06-10 12:21:48 +0800

    clean data - convert mistype data to int

--- data_preprocess/process.py
+++ data_preprocess/process.py
@@ -21,6 +21,27 @@ def remove_sleep_data():
         csv = pd.read_csv(path)
         csv = csv.drop(sleep_label, 1)
         csv.to_csv(path)
+
+def clean(path):
+    csv = pd.read_csv(path)
+    labels = csv.columns
+
+    for label in labels:
+        try:
+            valid = csv[label][~np.isnan(csv[label])]
+            if min(valid) < 1e-20:
+                csv[label][~np.isnan(csv[label])] = csv[label][~np.isnan(csv[label])].astype(int)
+
+        except Exception as e:
+            print(e)
+            continue
+    
+    csv.to_csv(path)
+    
+def main():
+    paths = ['Questionnaire.csv', 'Demographics.csv', 'Examination.csv', 'Laboratory.csv']
+    for path in paths:
+        clean(path)
     
 if __name__ == '__main__':
-    remove_sleep_data()
+    main()
